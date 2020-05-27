7a991218a2a2616c813516e3125ceef04d1b7cbd
asaern9 <asaern9@gmail.com>
2020-05-27 20:37:23 +0000

    model created

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+SEX = (
+    ('MALE','MALE'),
+    ('FEMALE', 'FEMALE')
+
+)
+
+DRUG_FORM = (
+    ('CAPSULE', 'CAPSULE'),
+    ('TABLET', 'TABLET'),
+    ('OINTMENT', 'OINTMENT'),
+    ('INJECTION', 'INJECTION')
+)
+
+CHANNEL = (
+    ('ORALLY', 'ORALLY'),
+    ('INTRAVENOUS', 'INTRAVENOUS')
+)
+
+
+class Prescription(models.Model):
+    patient_name = models.CharField(max_length=150)
+    patient_age = models.IntegerField()
+    patient_sex = models.CharField(max_length=8, choices=SEX)
+    patient_weight = models.FloatField(null=True)
+    patient_contact = models.CharField(max_length=10)
+    patient_date_of_birth = models.DateField(null=True)
+    postal_address = models.CharField(max_length=150, null=True)
+    drug_class = models.CharField(max_length=150, null=True)
+    drug_name = models.CharField(max_length=150)
+    drug_form = models.CharField(max_length=50, choices=DRUG_FORM)
+    channel = models.CharField(max_length=50, choices=CHANNEL)
+    quantity = models.IntegerField()
+    dosage = models.TextField()
+    precaution = models.TextField(null=True)
+    side_effect = models.TextField(null=True)
+    date_of_issue = models.DateField()
+    end_date = models.DateField()
+    administer_name = models.CharField(max_length=150)
+    administer_contact = models.CharField(max_length=10)
+    name_of_pharmacy = models.CharField(max_length=150)
+    pharmacy_address = models.CharField(max_length=150)
